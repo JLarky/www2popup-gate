7003f0abaac49a04922f43235fda11becaf6c70d
JLarky <jlarky@jlarky.info>
2008-08-12 13:16:42 +0400

    Теперь sendlater работает через mysql

---
+++ bin/sender.php
@@ -0,0 +1,44 @@
+<?php
+################
+# MySQL connect
+	include("../inc/config.php");
+	mysql_connect($INFO['base_host'],$INFO['base_user'],$INFO['base_pass']);
+	mysql_query("SET NAMES 'utf8'");
+	mysql_select_db($INFO['base_name']);
+	$error=mysql_error();echo ($error ? $error.__LINE__ : '');
+	
+################
+# Libs
+	include "../lib/popup-utils.php";
+
+################
+# Get list to send
+	$query="SELECT * from `".$INFO['later_tabl']."`";
+	$sql_res=mysql_query($query);
+	$error=mysql_error();echo ($error ? $error.__LINE__ : '');
+	while ($row=mysql_fetch_array($sql_res)) {
+		$from=$row['from'];
+		$to=$row['to'];
+		$msg=$row['msg'];
+		$content="";
+		$res=send_popup($from, $to, $msg);
+		$errs=Array();
+		$res=explode("\n", $res);
+		$ok=false;
+		foreach ($res as $str) {
+			if (mb_ereg(".*([0-9]+)/([0-9]+) were sent.*", $str, $reg_srt) && $reg_srt[1]) 
+				{ $ok=true;break; }
+		}
+		        if ($ok) {
+                        // Succsess
+                        mysql_query("DELETE FROM `".$INFO['later_tabl']."` WHERE `id`='".intval($row['id'])."'");
+                        } else {
+                        // Error
+                        $log=mysql_real_escape_string(join("\n", $res));
+                        mysql_query("UPDATE `".$INFO['later_tabl']."` SET `counter`=`counter`+1, `comment`='$log'  WHERE
+`id`='".intval($row['id'])."'");
+                        }
+
+	}
+
+?>
--- inc/sendlater.php
+++ inc/sendlater.php
@@ -1,10 +1,10 @@
 <?php
 function sendlater($to, $from, $msg, $time=null) {
   global $INFO;
-var_dump($to, $from, $msg, 'Пока не работает');
+
  $to=mysql_real_escape_string($to);
  $from=mysql_real_escape_string($from);
- $msg=mysql_real_escape_string($msg);
+ $msg=mysql_real_escape_string("Sended: ".date("d.m.y H:i:s")."\n".$msg);
  mysql_query("INSERT INTO `".$INFO['later_tabl']."` (`from`, `to`, `msg`, `counter`, `comment`)
 VALUES ('$from', '$to', '$msg', '0', 'Test') ;");
  echo mysql_error();
@@ -16,23 +16,7 @@ class PopupSender {
  private $msgfrom;
  private $msg;
  function __construct($msgto, $msgfrom, $msg) {
-  $this->msgto=escapeshellcmd($msgto);
-  $this->msgfrom=escapeshellcmd($msgfrom);
-  $this->msg=$msg;
-
-$tmpfname = tempnam("/tmp/psend", "sendl8_");
-chmod($tmpfname, 444);
-
-$handle = fopen($tmpfname, "w");
-fwrite($handle, "Sended: ".date("d.m.y H:i:s"));
-fwrite($handle, "\n");
-fwrite($handle, stripslashes($this->msg));
-fclose($handle);
-system("touch /var/www/gate/bin/tosend.add");
-system("chmod a+r /var/www/gate/bin/tosend.add");
-$handle = fopen("/var/www/gate/bin/tosend.add", "a");
-fwrite($handle, $this->msgto." ".$this->msgfrom." ".substr($tmpfname,strlen("/tmp/psend/"))."\n");
-fclose($handle);
+   sendlater($msgto, $msgfrom, $msg);
 }
 }
 ?>
